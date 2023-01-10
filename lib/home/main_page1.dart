afc86be561dfa65e830ef6f9c7fd4766ed109c98
Vasyl Puziichuk <ipzk221_pvv@student.ztu.edu.ua>
2023-01-10 21:28:05 +0200

    Day 2

---
+++ lib/home/main_page1.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:testappfirst/home/page_body1.dart';
+import 'package:testappfirst/untils/colors.dart';
+import 'package:testappfirst/widgets/big_taxt.dart';
+import 'package:testappfirst/widgets/small_text.dart';
+import 'package:testappfirst/untils/dimensions.dart';
+
+
+class MainFoodPage extends StatefulWidget {
+  const  MainFoodPage ({Key? key}) : super(key: key);
+
+  @override
+  _MainFoodPageState createState() =>  _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    //print("current height is "+MediaQuery.of(context).size.height.toString());
+    return Scaffold(
+      body: Column(
+        children: [
+          //Відображення шапки
+          Container(
+
+            child: Container(
+              margin: EdgeInsets.only(top: Dimensions.height55, bottom: Dimensions.height15),
+              padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Ukraine", color: AppColors.mainColor, size: 25,),
+                      Row(
+                        children: [
+                          SmallText(text: "Zhytomyr", color: Colors.black54,),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: Dimensions.height45,
+                      height: Dimensions.height45,
+                      child: Icon(Icons.search, color:Colors.white, size: Dimensions.iconSize28,),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.radius15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          //Відображення тіла головного вікна
+          FoodPageBody(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/untils/dimensions.dart
@@ -0,0 +1,35 @@
+import 'package:get/get.dart';
+
+class Dimensions{
+  //820
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight/2.65;//2.56
+  static double pageViewContainer = screenHeight/3.89;//3.72
+  static double pageViewTextContainer = screenHeight/7.08;//7.03
+  //динамічна висота відступів
+  static double height10=screenHeight/84.72;//82
+  static double height15=screenHeight/56.32;//51
+  static double height20=screenHeight/42.52;//41
+  static double height30=screenHeight/28.45;//41
+  static double height45=screenHeight/17;//41
+  static double height55=screenHeight/15;
+
+
+  //динамічна ширина відступів
+  static double width10=screenHeight/84.72;//82
+  static double width15=screenHeight/56.32;//51
+  static double width20=screenHeight/42.52;//41
+  static double width30=screenHeight/28.45;//41
+
+  static double font20=screenHeight/42.52;
+
+  static double radius15=screenHeight/56.6;
+  static double radius20=screenHeight/42.52;
+  static double radius30=screenHeight/28.18;
+
+  //розміри іконок
+  static double iconSize28 = screenHeight/35;
+
+}
