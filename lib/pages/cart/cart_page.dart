a3d703013105443963fb4d91a82109d2bc9285df
Vasyl Puziichuk <ipzk221_pvv@student.ztu.edu.ua>
2023-01-13 10:49:32 +0200

    UI markup for the cart page

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:testappfirst/untils/app_constants.dart';
 import 'package:testappfirst/untils/colors.dart';
 import 'package:testappfirst/untils/dimensions.dart';
+import 'package:testappfirst/widgets/big_taxt.dart';
+import 'package:testappfirst/widgets/small_text.dart';
+import 'package:get/get.dart';
 
+import '../../controllers/cart_controller.dart';
 import '../../widgets/app_icon.dart';
 
 class CartPage extends StatelessWidget {
@@ -44,7 +50,87 @@ class CartPage extends StatelessWidget {
               right: Dimensions.width20,
               bottom: 0,
               child: Container(
+                margin: EdgeInsets.only(top: Dimensions.height15),
+                //color: Colors.red,
+                child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: GetBuilder<CartController>(builder: (cartControlle){
+                    return ListView.builder(
+                        itemCount: cartControlle.getItems.length,
+                        itemBuilder: (_, index){
+                          return Container(
+                            width: double.maxFinite,
+                            height: Dimensions.height20*5,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: Dimensions.height20*5,
+                                  height: Dimensions.height20*5,
+                                  margin: EdgeInsets.only(bottom: Dimensions.height10),
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image:NetworkImage(
+                                            AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartControlle.getItems[index].img!
+                                          )
+                                      ),
+                                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                                      color: Colors.white
+                                  ),
+                                ),
+                                SizedBox(width: Dimensions.width10,),
+                                Expanded(
+                                    child: Container(
+                                      height: Dimensions.height20*5,
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          BigText(text: cartControlle.getItems[index].name!, color: Colors.black54,),
+                                          SmallText(text: "Spicy"),
+                                          Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              BigText(text: cartControlle.getItems[index]!.price.toString(), color: Colors.redAccent),
+                                              Container(
+                                                padding: EdgeInsets.only(top: Dimensions.height10,
+                                                    bottom: Dimensions.height10, left:Dimensions.width10,
+                                                    right: Dimensions.width10),
+                                                decoration: BoxDecoration(
+                                                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                                                  color: Colors.white,
+                                                ),
+                                                child: Row(
+                                                  children: [
+                                                    GestureDetector(
+                                                        onTap: (){
+                                                          //popularProduct.setQuantity(false);
+                                                        },
+                                                        child: Icon(Icons.remove, color: AppColors.signColor,)),
+                                                    SizedBox(width: Dimensions.width10/2,),
+                                                    BigText(text: "0"),//popularProduct.inCartItems.toString()),
+                                                    SizedBox(width: Dimensions.width10/2,),
+                                                    GestureDetector(
+                                                        onTap: (){
+                                                          //popularProduct.setQuantity(true);
+                                                        },
+                                                        child: Icon(Icons.add, color: AppColors.signColor,)),
+                                                  ],
+                                                ),
+                                              )
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ))
+                              ],
+                            ),
+                          );
 
+                        });
+                  }),
+                )
               ))
         ],
       ),
