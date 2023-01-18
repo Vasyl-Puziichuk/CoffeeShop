15d9c28c9f99a972ad5eb231497789489f9a0dad
Vasyl Puziichuk <ipzk221_pvv@student.ztu.edu.ua>
2023-01-18 19:51:39 +0200

    Method Getbuilder for sign up page and build custom loader class

--- lib/pages/auth/sing_up_page.dart
+++ lib/pages/auth/sing_up_page.dart
@@ -9,6 +9,7 @@ import 'package:testappfirst/untils/colors.dart';
 import 'package:testappfirst/untils/dimensions.dart';
 import 'package:testappfirst/widgets/big_taxt.dart';
 
+import '../../base/custom_loader.dart';
 import '../../widgets/app_text_field.dart';
 
 class SingUpPage extends StatelessWidget {
@@ -26,8 +27,8 @@ class SingUpPage extends StatelessWidget {
       "twit.png",
     ];
 
-    void _registration(){
-      var authController = Get.find<AuthController>();
+    void _registration(AuthController authController){
+      //var authController = Get.find<AuthController>();
       String name=nameController.text.trim();
       String phone=phoneController.text.trim();
       String email=emailController.text.trim();
@@ -71,108 +72,111 @@ class SingUpPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            SizedBox(height: Dimensions.screenHeight*0.055,),
-            //app logo
-            Container(
-              height: Dimensions.screenHeight*0.25,
-              child: Center(
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 80,
-                  backgroundImage: AssetImage(
-                    "assets/image/logo3.png"
-                  ),
-                ),
-              ),
-            ),
-            //email
-            AppTextField(
-                textController: emailController,
-                hintText: "Email...",
-                icon: Icons.email),
-            SizedBox(height: Dimensions.height20,),
-            //пароль
-            AppTextField(
-                textController: passwordController,
-                hintText: "Пароль",
-                icon: Icons.password_sharp),
-            SizedBox(height: Dimensions.height20,),
-            //ім'я
-            AppTextField(
-                textController: nameController,
-                hintText: "Ім'я",
-                icon: Icons.person),
-            SizedBox(height: Dimensions.height20,),
-            //телефон
-            AppTextField(
-                textController: phoneController,
-                hintText: "Телефон",
-                icon: Icons.phone_android_sharp),
-            SizedBox(height: Dimensions.height20+Dimensions.height20,),
-            //кнопка реєстрації
-            GestureDetector(
-              onTap: (){
-                _registration();
-              },
-              child: Container(
-                width: Dimensions.screenWidth/2,
-                height: Dimensions.screenHeight/12.5,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(Dimensions.radius30),
-                  color: AppColors.mainColor
-                ),
+      body: GetBuilder<AuthController>(builder: (_authCController){
+        return !_authCController.isLoading?SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              SizedBox(height: Dimensions.screenHeight*0.055,),
+              //app logo
+              Container(
+                height: Dimensions.screenHeight*0.25,
                 child: Center(
-                  child: BigText(
-                      text: "Доєднатися",
-                    size: Dimensions.font20+Dimensions.font20/2,
-                    color: Colors.white,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 80,
+                    backgroundImage: AssetImage(
+                        "assets/image/logo3.png"
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: Dimensions.height10,),
-            //існуючий акаунт
-            RichText(
-                text: TextSpan(
-                  recognizer: TapGestureRecognizer()..onTap=()=>Get.back(),
-                  text: "Вже маєте обліковий запис?",
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: Dimensions.font20,
-                    )
-                )
-            ),
-            SizedBox(height: Dimensions.screenHeight*0.035,),
-            //реєстрація по соціальних мережах
-            RichText(
-                text: TextSpan(
-                    text: "Зареєструйтесь одним із наведених нижче способів",
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: Dimensions.font16,
-                    )
-                )
-            ),
-            Wrap(
-              children: List.generate(3, (index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                    radius: Dimensions.radius30,
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage(
-                    "assets/image/"+singUpImage[index]
+              //email
+              AppTextField(
+                  textController: emailController,
+                  hintText: "Email...",
+                  icon: Icons.email),
+              SizedBox(height: Dimensions.height20,),
+              //пароль
+              AppTextField(
+                  textController: passwordController,
+                  hintText: "Пароль",
+                  icon: Icons.password_sharp),
+              SizedBox(height: Dimensions.height20,),
+              //ім'я
+              AppTextField(
+                  textController: nameController,
+                  hintText: "Ім'я",
+                  icon: Icons.person),
+              SizedBox(height: Dimensions.height20,),
+              //телефон
+              AppTextField(
+                  textController: phoneController,
+                  hintText: "Телефон",
+                  icon: Icons.phone_android_sharp),
+              SizedBox(height: Dimensions.height20+Dimensions.height20,),
+              //кнопка реєстрації
+              GestureDetector(
+                onTap: (){
+                  _registration(_authCController);
+                },
+                child: Container(
+                  width: Dimensions.screenWidth/2,
+                  height: Dimensions.screenHeight/12.5,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius30),
+                      color: AppColors.mainColor
+                  ),
+                  child: Center(
+                    child: BigText(
+                      text: "Доєднатися",
+                      size: Dimensions.font20+Dimensions.font20/2,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              )),
-            )
+              ),
+              SizedBox(height: Dimensions.height10,),
+              //існуючий акаунт
+              RichText(
+                  text: TextSpan(
+                      recognizer: TapGestureRecognizer()..onTap=()=>Get.back(),
+                      text: "Вже маєте обліковий запис?",
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                        fontSize: Dimensions.font20,
+                      )
+                  )
+              ),
+              SizedBox(height: Dimensions.screenHeight*0.035,),
+              //реєстрація по соціальних мережах
+              RichText(
+                  text: TextSpan(
+                      text: "Зареєструйтесь одним із наведених нижче способів",
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                        fontSize: Dimensions.font16,
+                      )
+                  )
+              ),
+              Wrap(
+                children: List.generate(3, (index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: Dimensions.radius30,
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage(
+                        "assets/image/"+singUpImage[index]
+                    ),
+                  ),
+                )),
+              )
 
-          ],
-        ),
-      ),
+            ],
+          ),
+        )
+            :const CustomLoader();
+      }),
     );
 
 
